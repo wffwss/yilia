862cd822e1e3a59e23e0a234b760cd78f2e0bcda
zziaguan <zziaguan@163.com>
2016-03-16 19:34:58 +0800

    2016年3月16日19:34:56

--- source/js/main.js
+++ source/js/main.js
@@ -72,7 +72,7 @@ require([], function (){
 	//是否开启动画
 	if(yiliaConfig.animate === true){
 
-		require(['/js/jquery.lazyload.js'], function(){
+		require(['http://apps.bdimg.com/libs/jquery-lazyload/1.9.5/jquery.lazyload.js'], function(){
 			//avatar
 			$(".js-avatar").attr("src", $(".js-avatar").attr("lazy-src"));
 			$(".js-avatar")[0].onload = function(){
